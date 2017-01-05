f511d263a319fd5f09323f850c49a5732e178896
Naomi Rubin <naomi@castironcoding.com>
2017-01-05 15:42:14 -0800

    [C] Temporarily configure to use Heroku again

--- lita_config.rb
+++ lita_config.rb
@@ -24,13 +24,13 @@ Lita.configure do |config|
   # config.adapter.password = "secret"
 
   ## Example: Set options for the Redis connection.
-  config.redis.host = ENV["BOXEN_REDIS_HOST"]
-  config.redis.port = ENV["BOXEN_REDIS_PORT"]
+  # config.redis.host = ENV["BOXEN_REDIS_HOST"]
+  # config.redis.port = ENV["BOXEN_REDIS_PORT"]
 
   ## Heroku Redis To-Go connection
   # Uncomment to use Heroku configuration
-  # config.redis[:url] = ENV["REDISTOGO_URL"]
-  # config.http.port = ENV["PORT"]
+  config.redis[:url] = ENV["REDISTOGO_URL"]
+  config.http.port = ENV["REDISTOGO_PORT"]
 
   ## Example: Set configuration for any loaded handlers. See the handler's
   ## documentation for options.
